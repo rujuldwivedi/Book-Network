4c9d9b9a481469f9a4fec31cc23ce49d7db039d0
Rujul Dwivedi <rujuldwivedi@icloud.com>
2024-10-18 11:52:03 +0530

    Delete book-network directory

--- book-network/src/main/java/com/alibou/book/exception/ActivationTokenException.java
+++
@@ -1,7 +0,0 @@
-package com.alibou.book.exception;
-
-public class ActivationTokenException extends RuntimeException {
-    public ActivationTokenException(String message) {
-        super(message);
-    }
-}
